f7885f8c3e318e69d6e7bb6285ba4ad4cc16349e
dolonmim <106053761+dolonmim@users.noreply.github.com>
2022-08-08 13:02:13 +0600

    Add files via upload

---
+++ user2/model/userModel.php
@@ -0,0 +1,77 @@
+<?php 
+
+    require_once('database.php');
+
+
+	function login( $email, $password){
+    $conn = getConnection();
+	$sql = "select * from users where email='{$email}' and password='{$password}'";
+	$result = mysqli_query($conn, $sql);
+	$count = mysqli_num_rows($result);
+
+ 	return $result;
+}
+
+
+function signup($id, $name, $gender, $dob, $email, $password, $address)
+	{
+		$conn = getConnection();
+		
+		$sql = "INSERT INTO users (id, name, gender, dob, email, password, address) VALUES ('{$id}','{$name}','{$gender}','{$dob}','{$email}','{$password}','{$address}')";
+		echo $sql;
+		if(mysqli_num_rows($matching))
+		{
+			
+			function_alert("Email Already Exist");
+			//echo '<script>alert("Email Already Exist")</script>';
+			header('location: ../view/signup.php');
+		}
+		else
+		{
+			if(mysqli_query($conn, $sql)){
+			return true;
+			}
+			else{
+			return false;
+			}
+		} 
+	}
+
+
+
+	function delete($id)
+	{
+		$conn = getConnection();
+		$sql= "Delete from users where ID='$id'";
+		//echo $sql;
+		$result=mysqli_query($conn,$sql);
+			if($result==TRUE)
+			{
+				header('location:../view/login.html');
+			}
+	
+	}
+
+
+	function updateuser($user){
+		$conn = getConnection();
+		
+		$sql = "update users SET   Name='{$user['Name']}', Email='{$user['Email']}', Password='{$user['Password']}', Address='{$user['Address']}' WHERE ID= {$user['ID']}";
+
+		echo $sql;
+		
+	
+		if(mysqli_query($conn, $sql)){
+			return ;
+		}else{
+			return false;
+		}
+	}
+
+
+	
+
+	
+	
+	
+?>
