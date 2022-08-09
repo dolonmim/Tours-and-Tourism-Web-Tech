00addf08346ee79584b0d12392ace88865c52fb6
dolonmim <106053761+dolonmim@users.noreply.github.com>
2022-08-09 21:22:49 +0600

    Add files via upload

---
+++ user/view/search.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<form action="../controller/searchcheck.php" method="post" enctype="" >
+		
+		<fieldset >
+			<legend> SEARCH </legend>
+			<table align="center">
+				<tr>
+					<td>SEARCH</td>
+					<td><input type="text"  name="SEARCH" value=""></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" name="" value="SEARCH"></td>
+				</tr>
+			</table>
+
+		</fieldset>
+
+	</form>
+
+
+
+            </table>
+        </fieldset>
+        </from>
+        
+            
+       
+
+</body>
+
+</html>
