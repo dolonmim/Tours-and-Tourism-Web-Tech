dd133b8970b3b09a9193037c6763a32e52de3255
dolonmim <106053761+dolonmim@users.noreply.github.com>
2022-08-22 13:25:50 +0600

    Add files via upload

---
+++ user/controller/paymentcheck.php
@@ -0,0 +1,33 @@
+<?php 
+	session_start();
+	require('../model/database.php');
+	require('../model/userModel.php');
+
+	if(isset($_REQUEST['submit']))
+	{
+		$email=$_POST['email'];
+		$name=$_POST['name'];
+		$h_m_day=$_POST['h_m_day'];
+		$package=$_POST['package'];
+		$amount=$_POST['amount'];
+		
+
+		
+
+							if($email != null && $name != null && $h_m_day != null && $package != null && $amount != null )
+								{
+									
+										$status = paymentsignup($email, $name,$h_m_day, $package,$amount);
+											if($status)
+											{
+												header('location: ../view/feedback.html');
+											}else
+											{
+												header('location: ../view/payment.php');
+											}
+								}
+								
+		}
+	
+	
+?>
---
+++ user/view/payment.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="style2.css">
+</head>
+	<body>
+		<h2></h2>
+		<div class="signup">
+		<div class="navigation">
+
+			 <div class="back">
+				<ul>
+					<li><a href="about.html">ABOUT</a></li>
+					<li><a href="contact.html">CONTACT</a></li>
+					<li><a href="loginview.php">BACK</a></li>
+				</ul>
+			 </div>
+			</div>
+       <form  onsubmit="return validate()" action="../controller/paymentcheck.php" method="POST">
+        
+		
+			
+                
+                <center><table>
+                <tr>
+                        
+                        <td><b>Email</b> </td>
+                        </tr>
+                        <tr>
+                        <td><input type="email" name="email" placeholder="Enter your Email" value="" id="email"></td>
+                    </tr>
+                
+                        <td><b>Name</b> </td>
+                        </tr>
+                        <tr>
+                        <td><input type="text" name="name" placeholder="Enter your destination" value="" id="name"></td>
+                    </tr>
+                    
+                    <tr>
+                        <td><b>Days</b></td>
+                        </tr>
+                        <tr>
+                        <td><input type="text" name="h_m_day" placeholder="How long will you stay" value="" id="h_m_day"></td>
+                    </tr>
+                    <tr>
+                        <td><b>Package</b></td>
+                        </tr>
+                        <tr>
+                        <td><input type="text" name="package" placeholder="package" value="" id="package"></td>
+                    </tr>
+                    <tr>
+                        <td><b>Amount</b></td></tr>
+                        <tr>
+                        <td >
+                        <input type="text" name="amount" placeholder="Amount" value="" id="amount"  >
+                        </td>
+                    </tr>
+                    
+                    
+                    
+                    
+					<tr>
+						
+                        <td>
+							<div class="signupbtn">
+								<input type="submit" name="submit" value=" BOOK"></div>
+								
+                        
+                    </tr>
+                    
+
+                      </table>
+</center>
+           
+        </form>
+	
+
+
+		</div>
+
+
+
+              
+</body>
+</html>
