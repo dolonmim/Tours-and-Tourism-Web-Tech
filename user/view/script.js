c5f1e08f7892b6a46a0c781d37e35311f5f56953
dolonmim <106053761+dolonmim@users.noreply.github.com>
2022-08-23 22:22:23 +0600

    Add files via upload

---
+++ user/view/script.js
@@ -0,0 +1,18 @@
+var jcontent=
+{
+    "name": "Emma Watson",
+    "email":"emma@gmail.com",
+    "category":"Content Creater"
+}
+var output=document.getElementById('output');
+output.innerHTML=jcontent.name +'     ' + jcontent.email + '    '+jcontent.category;
+
+
+var jcontent2=
+{
+    "name": "Danny Rang",
+    "email":"danny@gmail.com",
+    "category":"Web Designer"
+}
+var output=document.getElementById('output2');
+output.innerHTML=jcontent2.name +'  ' + jcontent2.email + '  '+jcontent2.category;
